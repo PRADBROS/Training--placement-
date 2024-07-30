2c7695c03a25e93b2751f35826d95f5ff9569702
PRADBROS <151357767+PRADBROS@users.noreply.github.com>
2024-07-30 21:54:41 +0530

    Create Elementexist.py

---
+++ 30.07.2024/Elementexist.py
@@ -0,0 +1,6 @@
+arr = [1, 2, 3, 4, 5]
+element_to_check = int(input("Enter the element to check: "))
+if element_to_check in arr:
+    print(f"{element_to_check} exists in the array")
+else:
+    print(f"{element_to_check} does not exist in the array")
