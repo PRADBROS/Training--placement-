dbc59cdf034480990a7ade0e89f512eebadfc362
PRADBROS <151357767+PRADBROS@users.noreply.github.com>
2024-08-07 22:26:53 +0530

    Create Template.py

---
+++ 07.08.2024/Template.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+  <!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Home</title>
+  </head>
+  <body>
+    <h1>Welcome to My Web Page</h1>
+  </body>
+</html>
